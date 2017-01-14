ff196b9e51082ed15ecb7b24a5633d39fc0702ed
GIGA6600KI5\jjate <jjatem@hotmail.com>
2017-01-13 23:03:04 -0500

    adding some code changes to trigger a jenkins build

--- tutorial/Controllers/ProductController.cs
+++ tutorial/Controllers/ProductController.cs
@@ -25,8 +25,49 @@ namespace tutorial.Controllers
         }
 
         // POST api/<controller>
-        public void Post([FromBody]string value)
+        public void Post([FromBody] dynamic value)
         {
+            string iProductName = value;
+
+            if (value != null && (value as string).Length > 40)
+            {
+                iProductName = iProductName.Substring(0, 40);
+            }
+
+            var NewProduct = db.Products.Add(new Product()
+            {
+                CategoryID = 1,
+                SupplierID = 1,
+                ProductName = iProductName,
+                QuantityPerUnit = "99 boxes",
+                UnitPrice = 100,
+                UnitsInStock = 10,
+                UnitsOnOrder = 0,
+                ReorderLevel = 5,
+                Discontinued = false
+            });
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (System.Data.Entity.Validation.DbEntityValidationException dbEx)
+            {
+                Exception raise = dbEx;
+                foreach (var validationErrors in dbEx.EntityValidationErrors)
+                {
+                    foreach (var validationError in validationErrors.ValidationErrors)
+                    {
+                        string message = string.Format("{0}:{1}",
+                            validationErrors.Entry.Entity.ToString(),
+                            validationError.ErrorMessage);
+                        // raise a new exception nesting
+                        // the current instance as InnerException
+                        raise = new InvalidOperationException(message, raise);
+                    }
+                }
+                throw raise;
+            }
         }
 
         // PUT api/<controller>/5
